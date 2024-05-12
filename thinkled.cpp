b77aa603742df18257fda3a1d833d812a4c33688
ymcx <89810988+ymcx@users.noreply.github.com>
2024-05-12 17:28:17 +0300

    add the code

---
+++ thinkled.cpp
@@ -0,0 +1,39 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+
+#include <stdint.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/io.h>
+
+void send_ec_data(uint8_t data) {
+  int timeout = 0x7ff;
+  while ((inb(0x66) & 2) && --timeout) {
+    usleep(10);
+  }
+  outb(data, 0x62);
+}
+
+void ec_write(uint8_t data) {
+  outb(0x81, 0x66);
+  send_ec_data(12);
+  send_ec_data(data);
+}
+
+int main() {
+  iopl(3);
+  FILE * file = fopen("/sys/class/power_supply/BAT0/capacity", "r");
+  int battery = 0;
+  while (1) {
+    fscanf(file, "%i", &battery);
+    if (battery <= 10) {
+      ec_write(192);
+    } else if (battery <= 50) {
+      ec_write(160);
+    } else {
+      ec_write(128);
+    }
+    sleep(60);
+  }
+  fclose(file);
+  return 0;
+}
