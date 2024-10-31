cf7f015b46c50e5c27e42ed50e03b652c86510a4
user <89810988+ymcx@users.noreply.github.com>
2024-10-31 10:52:47 +0200

    instead of relying on ec, write to /proc/acpi

--- thinkled.cpp
+++ thinkled.cpp
@@ -1,36 +1,21 @@
 /* SPDX-License-Identifier: GPL-2.0-only */
 
-#include <iostream>
-#include <stdint.h>
+#include <cstdlib>
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/io.h>
 
-void send_ec_data(uint8_t data) {
-  int timeout = 0x7ff;
-  while ((inb(0x66) & 2) && --timeout) {
-    usleep(10);
-  }
-  outb(data, 0x62);
-}
-
-void ec_write(uint8_t data) {
-  outb(0x81, 0x66);
-  send_ec_data(12);
-  send_ec_data(data);
-}
-
-int main(int argc, char * argv[]) {
-  iopl(3);
+int main() {
   int data;
-  if (argv[1] == NULL) {
-    FILE * file = fopen("/sys/class/power_supply/BAT0/capacity", "r");
-    fscanf(file, "%i", &data);
-    fclose(file);
-    data = 192 - (data + 34) / 45 * 32;
-  } else {
-    data = atoi(argv[1]);
+  FILE * file = fopen("/sys/class/power_supply/BAT0/capacity", "r");
+  fscanf(file, "%i", &data);
+  fclose(file);
+  if (data < 10) {
+    system("echo '0 blink' > /proc/acpi/ibm/led");
   }
-  ec_write(data);
+  else if (data < 50) {
+    system("echo '0 on' > /proc/acpi/ibm/led");
+  }
+  else {
+    system("echo '0 off' > /proc/acpi/ibm/led");
+  }
   return 0;
 }
