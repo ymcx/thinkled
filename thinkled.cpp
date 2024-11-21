d4cbf40dca6a1aac4ea4b646ce0c0f3962029755
ymcx <89810988+ymcx@users.noreply.github.com>
2024-11-21 18:33:35 +0200

    modify the led on the back as well

--- thinkled.cpp
+++ thinkled.cpp
@@ -10,12 +10,15 @@ int main() {
   fclose(file);
   if (data <= 5) {
     system("echo '0 blink' > /proc/acpi/ibm/led");
+    system("echo '10 blink' > /proc/acpi/ibm/led");
   }
   else if (data <= 50) {
     system("echo '0 on' > /proc/acpi/ibm/led");
+    system("echo '10 on' > /proc/acpi/ibm/led");
   }
   else {
     system("echo '0 off' > /proc/acpi/ibm/led");
+    system("echo '10 off' > /proc/acpi/ibm/led");
   }
   return 0;
 }
